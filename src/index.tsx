c95af9d46364afb5a3b48ebb3d7728aa81b180dd
sudo888samewick <none>
2023-12-03 18:17:39 +0000

    实现到分享链接，并且把黑暗白昼模式也写了

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import { Bars3Icon, XMarkIcon, DocumentDuplicateIcon as CopyIcon, ShareIcon } from '@heroicons/react/24/outline'
+import 'flowbite'
+import 'flowbite/dist/flowbite.css'
 
 import React from 'react';
+
 import './css/tailwind.css'
+import ShareLogo from './assets/logos.png'
+
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
@@ -48,9 +52,11 @@ function App() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 dark:text-slate-50 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+            <button id="theme-toggle" type="button" className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+              <svg id="theme-toggle-dark-icon" className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+              <svg id="theme-toggle-light-icon" className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+            </button>
+
           </div>
         </nav>
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -87,21 +93,21 @@ function App() {
                     </a>
                   ))}
                 </div>
-                <div className="py-6">
+                {/* <div className="py-6">
                   <a
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 dark:text-slate-50 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
                   </a>
-                </div>
+                </div> */}
               </div>
             </div>
           </Dialog.Panel>
         </Dialog>
       </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="relative isolate pt-14 ">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
@@ -114,34 +120,101 @@ function App() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 dark:text-slate-50 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+        <div className="mx-auto py-24 sm:py-32 lg:pb-40">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-7xl text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-slate-50 sm:text-6xl">
+                HulyAd: 复制粘贴 躺着赚钱
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-slate-50">
+                无需注册，只需转发链接，我们会将网站一半的收入分享给你，一切可验证，代码尽开源
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  试一试
+                </a>
+                <a href="#" className="text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50">
+                  了解更多 <span aria-hidden="true">→</span>
+                </a>
+              </div>
             </div>
+            <div className="mt-16 flow-root sm:mt-24">
+              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <img
+                  src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                  alt="App screenshot"
+                  width={2432}
+                  height={1442}
+                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                />
+              </div>
+            </div>
+
           </div>
-          <div className="text-center">
-            <h3 className="text-3xl font-bold tracking-tight dark:text-slate-50 text-gray-900 sm:text-6xl">
-              HulyAd: 一个去中心化广告系统
-            </h3>
-            <p className="mt-6 text-lg leading-8 dark:text-slate-50 text-gray-600">
-              无需注册，只需转发链接，我们会将网站一半的收入分享给你，一切可验证，代码尽开源
+        </div>
+        <div className="mx-auto max-w-4xl px-6 lg:px-8">
+          <div className="mx-auto max-w-6xl lg:text-center">
+            <h2 className="text-base font-semibold leading-7 text-indigo-600">Make Money in Minutes</h2> 
+            <p className="mt-2 pb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-slate-50 sm:text-4xl">
+              立刻开始部署
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900 dark:bg-slate-50">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
+
+
+            <ol className="relative text-gray-500 border-s border-gray-200 dark:border-gray-700 dark:text-gray-400 transform">
+              {
+                [{
+                  title: '起名字',
+                  content: <div className="flex max-w-xl mx-auto">
+                  <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                    https://
+                  </span>
+                  <input type="text" id="website-admin" className="bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="填入一个英文名字, 确认之后就无法修改哦" />
+                  <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-md border-gray-300 rounded-s-0 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                    .gptinfo.online
+                  </span>
+                  <div className='w-2'></div>
+                  <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">确认</button>
+                </div>,
+                  icon: CopyIcon
+                },
+                {
+                  title: '分享链接',
+                  content: <img
+                  src={ ShareLogo }
+                  alt="Share"
+                  width={2432}
+                  height={1442}
+                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                />,
+                  icon: ShareIcon
+                }
+                ].map(x => {
+                  const Icon = x.icon
+
+                  return <li className="mb-10 ms-6">
+                  <span className="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                    <Icon/>
+
+                  </span>
+                  <h3 className="leading-tight font-semibold text-left text-xl pb-2">{x.title}</h3>
+                    <div className="">
+                      {
+                        x.content
+                      }
+                    </div>
+
+                </li>})
+
+              }
+            </ol>
+
+
+
+          </div>
+          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           </div>
         </div>
         <div
